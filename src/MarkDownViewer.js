bad3a70eedfa4ea1e7b2b0d47254253c6de34868
Nobuyuki Mukai <ycuobu@gmail.com>
2019-11-16 19:19:19 +0900

    0.0.3

--- src/MarkDownViewer.js
+++ src/MarkDownViewer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Reserved, MarkdownTextBox } from '@3yaa3yaa/markdowntextbox';
+import { Map}  from 'treemindmap'
+import Img from 'gatsby-image'
 
 export default class MarkDownViewer extends Component {
 
@@ -12,8 +14,11 @@ export default class MarkDownViewer extends Component {
     {
         let original=(data)=>{return data.split('|')[0]}
         let reading=(data)=>{return data.split('|')[1]}
-        let out = new Reserved('rb(',')',(data,index)=>{return (<div key={index}><ruby>{original(data)}<rt>{reading(data)}</rt></ruby></div>)})
-        return [out];
+        let out=[]
+        out.push(new Reserved('rb(',')',(data,index)=>{return (<div key={index}><ruby>{original(data)}<rt>{reading(data)}</rt></ruby></div>)}))
+        out.push(new Reserved('^tmm:\n','^\n',(data,index)=>{return (<div key={index}><Map initialState={data}/></div>)}))
+        out.push(new Reserved('img(',')',(data,index)=>{return (<div key={index}><Img fixed={data}/></div>)}))
+        return out;
     }
 
     render() {
